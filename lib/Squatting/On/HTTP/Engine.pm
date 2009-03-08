738d7154106287a8b58b26c572590a1a6d79bccd
John Beppu <beppu@cpan.org>
2009-03-08 13:44:09 +0000

    Import of BEPPU/Squatting-On-HTTP-Engine-0.04 from CPAN.
    gitpan-cpan-distribution: Squatting-On-HTTP-Engine
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BEPPU/Squatting-On-HTTP-Engine-0.04.tar.gz
    gitpan-cpan-author:       BEPPU
    gitpan-cpan-maturity:     released

--- lib/Squatting/On/HTTP/Engine.pm
+++ lib/Squatting/On/HTTP/Engine.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use HTTP::Engine;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our %p;
 
 $p{e} = sub {
@@ -43,7 +43,10 @@ sub http_engine {
   my ($app, %options) = @_;
   $options{interface}{request_handler} = sub {
     my ($req)   = @_;
-    my ($c, $p) = &{ $app . "::D" }($req->uri->path);
+    my $u_path  = $req->uri->path;
+    my $path    = substr($u_path, 0, (length($u_path) >> 1)) if (length($u_path) > 1); # XXX - remove hack when H:E:I:FCGI gets fixed
+    $path     ||= $u_path;
+    my ($c, $p) = &{ $app . "::D" }($path);
     my $cc      = $p{init_cc}($c, $req);
     my $content = $app->service($cc, @$p);
     HTTP::Engine::Response->new(
